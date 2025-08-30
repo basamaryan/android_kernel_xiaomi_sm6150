777a8105707af00e3f904da8ece98bec6680d5f5
basamaryan <basam.aryan@gmail.com>
2025-08-31 00:22:38 +0200

    regen
    Change-Id: I96c80ea65f1b0a7409a99db7df2ba0ce3d70bea1

--- regen.sh
+++ regen.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # Array of device names
-devices=("sweet" "tucana" "toco" "phoenix" "davinci")
-#devices=("sweet" "courbet")
+devices=("sweet")
 
 # Loop through the devices
 for device in "${devices[@]}"
